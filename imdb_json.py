f01926679656586e310745c36e055178fe79678b
Hrishikesh Sathe <xxx2050@gmail.com>
2014-05-13 19:07:57 +0530

    added functions for getting info

--- imdb_json.py
+++ imdb_json.py
@@ -11,6 +11,77 @@ def get_content(url):
     r = requests.get(url)
     return r.text
 
+#function to get the overview info as mentioned on the IMDb page
+def get_overview(page_content):
+    #get the HTML code which gives an overview of the show/movie
+    start_index = page_content.find("overview-top")
+    end_index = page_content.find("overview-bottom")
+    
+    #pass it to soup
+    soup = BeautifulSoup(page_content[start_index:end_index])
+    
+    #create json object. check class JSONAttributes(). it just contains variables to store values
+    json_temp = json_attributes.JSONAttributes()
+    
+    #get the time tag. i.e. the duration of the show
+    tag = soup.time
+    
+    #get the genre of the show/movie
+    genre=soup.findAll(itemprop='genre')
+    
+    #get the rating of the show/movie
+    rating = soup.find(name='div',attrs={'class','star-box-giga-star'})
+    
+    #get a short description of the show/movie
+    description = soup.find(itemprop='description')
+    
+    #get the show/movie title and a short cast list
+    names = soup.findAll(itemprop='name')
+    
+    #set the values in the json object
+    json_temp.duration = tag.text.strip()
+    for item in genre:
+        json_temp.genre.append(item.text)
+        json_temp.rating = rating.text.strip()
+        json_temp.description = description.text.strip()
+        json_temp.title = names[0].text
+        for i in range(1,len(names)):
+            json_temp.actors.append(names[i].text)
+    return json_temp
+
+
+
+def get_seasons_info(page_content,json,url):
+    #traverse to get the number of seasons by searching number of links in the titleTVSeries block
+    start_index = page_content.find('titleTVSeries')
+    page_content = page_content[start_index:-1]
+    end_index = page_content.find('/div')
+    soup = BeautifulSoup(page_content[:end_index])
+    number_of_seasons = soup.findAll('a')
+    
+    # change the url and store in another variable
+    # usually imdb pages for seasons are in the format http://www.imdb.com//title/title_code/episode?season=n where n is a number
+    index = url.find('?')
+    new_url = url[:index]
+    #add the episode info to an object of season_info. add this object to the json_attributes.seasons(this is a list of seasons)
+    for i in range(0,len(number_of_seasons)):
+        season = season_info()
+        url = new_url+'episodes?season='+str(i+1)
+        season_content = get_content(url)
+        soup = BeautifulSoup(season_content)
+        episode_list = soup.find(name='div',attrs={'class','eplist'})
+        name = episode_list.findAll(itemprop='name')
+        airdate = episode_list.findAll(name='div',attrs={'class','airdate'})
+        description = episode_list.findAll(itemprop='description')
+        for j in range(0,len(name)):
+            season.season_number = i+1
+            season.episode_name.append(name[j].text.strip())
+            season.episode_airdate.append(airdate[j].text.strip())
+            season.episode_overview.append(description[j].text.strip())
+        json.seasons.append(season)
+    return json
+
+
 # ask the user for input. user can input show name or movie name.
 user_input = input("Enter the name of the movie/series to create the JSON")
 url = "http://www.imdb.com/find?q="+user_input
@@ -34,70 +105,7 @@ url = "http://www.imdb.com/"+tag['href']
 #get content from new url which will now be the show/movie page on IMDb
 content = get_content(url)
 
-#get the HTML code which gives an overview of the show/movie
-start_index = content.find("overview-top")
-end_index = content.find("overview-bottom")
-
-#pass it to soup
-soup = BeautifulSoup(content[start_index:end_index])
-
-#create json object. check class JSONAttributes(). it just contains variables to store values
-json = json_attributes.JSONAttributes()
-
-#get the time tag. i.e. the duration of the show
-tag = soup.time
-
-#get the genre of the show/movie
-genre=soup.findAll(itemprop='genre')
-
-#get the rating of the show/movie
-rating = soup.find(name='div',attrs={'class','star-box-giga-star'})
-
-#get a short description of the show/movie
-description = soup.find(itemprop='description')
-
-#get the show/movie title and a short cast list
-names = soup.findAll(itemprop='name')
-
-#set the values in the json object
-json.duration = tag.text.strip()
-for item in genre:
-    json.genre.append(item.text)
-json.rating = rating.text.strip()
-json.description = description.text.strip()
-json.title = names[0].text
-for i in range(1,len(names)):
-    json.actors.append(names[i].text)
-
-print("Overview info:-")
-json.print_overview()
-
-#traverse to get the number of seasons by searching number of links in the titleTVSeries block
-start_index = content.find('titleTVSeries')
-content = content[start_index:-1]
-end_index = content.find('/div')
-soup = BeautifulSoup(content[:end_index])
-seasons = soup.findAll('a')
-
-# change the url and store in another variable
-# usually imdb pages for seasons are in the format http://www.imdb.com//title/title_code/episode?season=n where n is a number
-index = url.find('?')
-new_url = url[:index]
-#add the episode info to an object of season_info. add this object to the json_attributes.seasons(this is a list of seasons)
-for i in range(0,2):
-    season = season_info()
-    url = new_url+'episodes?season='+str(i+1)
-    season_content = get_content(url)
-    soup = BeautifulSoup(season_content)
-    episode_list = soup.find(name='div',attrs={'class','eplist'})
-    name = episode_list.findAll(itemprop='name')
-    airdate = episode_list.findAll(name='div',attrs={'class','airdate'})
-    description = episode_list.findAll(itemprop='description')
-    for j in range(0,len(name)):
-        season.season_number = i+1
-        season.episode_name.append(name[j].text.strip())
-        season.episode_airdate.append(airdate[j].text.strip())
-        season.episode_overview.append(description[j].text.strip())
-    json.seasons.append(season)
-
+#get overview info and season+episode list
+json = get_overview(content)
+json = get_seasons_info(content,json,url)
 json.print_season_info()
